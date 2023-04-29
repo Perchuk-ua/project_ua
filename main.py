f86028af7bd803443b58cb29c73d38a0d76fd148
Perchuk-ua <demyan2010barabash@gmail.com>
2023-04-29 11:48:25 +0300

    car

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-class Perchuk:
-    def __init__(self, name, list, ):
+class Human:
+    def __init__(self, name):
         self.name = name
-        self.list = list
-    def average(self):
-        print(f"Averege: {sum(self.list) / len(self.list)}")
 
-s1 = Perchuk("Demian", [9, 12, 10, 8 ,5 , 10 , 4 , 7])
-s1.average()
+class Auto:
+    def __init__(self, brand):# ініт це конструктор
+        self.brand = brand
+        self.passengers = []
+    def add_passenger(self, human):
+        self.passengers.append(human)
+
+    def print_passengers(self):
+        if self.passengers != []:
+            print(f"B Mashuni {self.brand} taki pasaguru")
+            for pasenger in self.passengers:
+                print(pasenger.name)
+        else:
+            print(f"B masuni {self.brand} нema nasaguriv. Bsi Bteklu")
+
+car = Auto("Toyota Supra")
+car.add_passenger(Human("Ilur"))
+car.add_passenger(Human("Igor"))
+car.add_passenger(Human("Maks"))
+car.add_passenger(Human("Shasha"))
+car.print_passengers()
