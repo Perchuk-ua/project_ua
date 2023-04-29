c48fafbcd0cc2cf7d6d6cbf4b31979c7c172bcbf
Perchuk-ua <demyan2010barabash@gmail.com>
2023-04-29 10:02:16 +0300

    class perchuk messege

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ class Perchuk:
         self.name = name
         self.surname = surname
         self.age = age
-
+    def info(self):
+        print(f"Student: {self.name} {self.surname}")
+    def massage(self, text):
+        print(f"Слава Україні, {self.name}. {text}")
 s1 = Perchuk("Demian", "Barabash", 35)
-print(s1.name)
-print(s1.surname)
-print(s1.age)
+s2 = Perchuk("ilur", "ilurovich", 25)
+s1.info()
+s2.info()
+s1.massage("Героям Слава")
