4d8e318f699fa4587d2a128331436f5d3ded8c8e
Perchuk-ua <demyan2010barabash@gmail.com>
2023-05-16 11:00:49 +0300

    дз

--- main.py
+++ main.py
@@ -1,32 +1,51 @@
-class Human:
-    def __init__(self, name):
+class Person:
+    def __init__(self, name, age, gender):
         self.name = name
+        self.age = age
+        self.gender = gender
 
-class Auto:
-    def __init__(self, brand, capacity):
-        self.brand = brand
-        self.capacity = capacity
-        self.passengers = []
-
-    def add_passenger(self, human):
-        if len(self.passengers) < self.capacity:
-            self.passengers.append(human)
-        else:
-            print(f"А шо ти?Ти зайвий")
-
-    def print_passengers(self):
-        if self.passengers:
-            print(f"В машині {self.brand} такі пасажири:")
-            for passenger in self.passengers:
-                print(passenger.name)
-        else:
-            print(f"В машині {self.brand} немає пасажирів. Всі втікли!")
+    def say_hello(self):
+        print(f"Hello, my name is {self.name} and I am {self.age}")
 
 
-car = Auto('Nissan GTR', 4)
-car.add_passenger(Human("Oleg"))
-car.add_passenger(Human("Igor"))
-car.add_passenger(Human("Vlad"))
-car.add_passenger(Human("Valentyn"))
-car.add_passenger(Human("Valentyn"))
-car.print_passengers()
+class Employee(Person):
+    def __init__(self, name, age, gender, salary, position):
+        super().__init__(name, age, gender)
+        self.salary = salary
+        self.position = position
+
+    def display_details(self):
+        super().say_hello()
+        print(f"Salary: {self.salary}")
+        print(f"Position: {self.position}")
+
+
+class Manager(Employee):
+    def __init__(self, name, age, gender, salary, position, num_subordinates):
+        super().__init__(name, age, gender, salary, position)
+        self.num_subordinates = num_subordinates
+
+    def display_details(self):
+        super().display_details()
+        print(f"Number of Subordinates: {self.num_subordinates}")
+
+
+class Developer(Employee):
+    def __init__(self, name, age, gender, salary, position, programming_language):
+        super().__init__(name, age, gender, salary, position)
+        self.programming_language = programming_language
+
+    def display_details(self):
+        super().display_details()
+        print(f"Programming Language: {self.programming_language}")
+
+
+# Приклад використання класів
+e = Employee("John", 30, "Male", 5000, "Employee")
+e.display_details()
+
+m1 = Manager("Smith", 35, "Female", 8000, "Manager", 15)
+m1.display_details()
+
+d1 = Developer("Mike", 28, "Male", 6000, "Developer", "Python")
+d1.display_details()
