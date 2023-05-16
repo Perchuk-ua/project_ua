7da6dca6a966708944f27acd244953430707c333
Perchuk-ua <demyan2010barabash@gmail.com>
2023-05-16 10:44:51 +0300

    дз

--- main.py
+++ main.py
@@ -1,97 +1,32 @@
-# try:
-#     код , який виконується
-# except:
-#     обробка вийнятку
+class Human:
+    def __init__(self, name):
+        self.name = name
 
-# error = "!"
-# try:
-#     print("start code")
-#     print(error)
-#     print("No error")
-# except:
-#     print("We have error")
+class Auto:
+    def __init__(self, brand, capacity):
+        self.brand = brand
+        self.capacity = capacity
+        self.passengers = []
 
-# print("code after capsule")
+    def add_passenger(self, human):
+        if len(self.passengers) < self.capacity:
+            self.passengers.append(human)
+        else:
+            print(f"А шо ти?Ти зайвий")
 
-# try:
-#     print("start code")
-#     print(a)
-#     print(10/0)
-#     print("No error")
-# except NameError:
-#     print("We have error")
-# except ZeroDivisionError:
-#     print("0 / !")
-#
-# print("code after capsule")
-
-# try:
-#     print("start code")
-#     print(a)
-#     print(10/0)
-#     print("No error")
-# except (NameError, ZeroDivisionError):
-#     print("We have error")
-#
-# print("code after capsule")
-
-# try:
-#     print("start code")
-#     print(a)
-#     print(10 / 0)
-#     print("No error")
-# except (NameError, ZeroDivisionError) as error:
-#     print(f"Error: {error}")
-#
-# print("code after capsule")
+    def print_passengers(self):
+        if self.passengers:
+            print(f"В машині {self.brand} такі пасажири:")
+            for passenger in self.passengers:
+                print(passenger.name)
+        else:
+            print(f"В машині {self.brand} немає пасажирів. Всі втікли!")
 
 
-
-# def checker(text):
-#     if type(text) != str:
-#         raise TypeError(f"Sorry, we can`t work with {type(text)}, we need calss STR!")
-#
-# try:
-#     checker(5)
-# except TypeError as error:
-#     print(error)
-
-# def calculator():
-#     print("Вітаю в калькуляторі!")
-#     while True:
-#         operation = input("Виберіть дію(+,-,*,/): ")
-#         if operation not in ['+', '-', '*', '/']:
-#             print("не правильна дія.Попробуйте ще раз.")
-#             continue
-#         n1 = float(input("Перше число: "))
-#         n2 = float(input("Друге числоу: "))
-#         try:
-#             if operation == '+':
-#                 result = n1 + n2
-#             elif operation == '-':
-#                 result = n1 - n2
-#             elif operation == '*':
-#                 result = n1 * n2
-#             elif operation == '/':
-#                 result = n1 / n2
-#             print("Result: ", result)
-#         except ZeroDivisionError:
-#             print("Error: division by zero")
-#             continue
-#             print(f"{n1} {operation} {n2} = {result}")
-#
-# calculator()
-#
-# 2 пара
-#
-# import logging
-#
-# logging.basicConfig(level=logging.DEBUG,
-# filename="logs.log", filemode="w",
-# format="[%(asctime)s] %(levelname)s %(message)s")
-# logging.debug("text")
-# logging.info("User123 succesfull logget to account 'login123'")
-# logging.info("text")
-# logging.warning("text")
-# logging.error("text")
-# logging.critical("text")
+car = Auto('Nissan GTR', 4)
+car.add_passenger(Human("Oleg"))
+car.add_passenger(Human("Igor"))
+car.add_passenger(Human("Vlad"))
+car.add_passenger(Human("Valentyn"))
+car.add_passenger(Human("Valentyn"))
+car.print_passengers()
