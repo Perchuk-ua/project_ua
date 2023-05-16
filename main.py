a2219335d6abda0fceadecde6ba270f41aacba63
Perchuk-ua <demyan2010barabash@gmail.com>
2023-05-16 10:41:22 +0300

    lesson 5-6

--- main.py
+++ main.py
@@ -1,124 +1,97 @@
-# class Human:
-#     height = 180
+# try:
+#     код , який виконується
+# except:
+#     обробка вийнятку
+
+# error = "!"
+# try:
+#     print("start code")
+#     print(error)
+#     print("No error")
+# except:
+#     print("We have error")
+
+# print("code after capsule")
+
+# try:
+#     print("start code")
+#     print(a)
+#     print(10/0)
+#     print("No error")
+# except NameError:
+#     print("We have error")
+# except ZeroDivisionError:
+#     print("0 / !")
 #
-# class Student(Human):
-#     pass
+# print("code after capsule")
+
+# try:
+#     print("start code")
+#     print(a)
+#     print(10/0)
+#     print("No error")
+# except (NameError, ZeroDivisionError):
+#     print("We have error")
 #
-# class Worker(Human):
-#     pass
+# print("code after capsule")
+
+# try:
+#     print("start code")
+#     print(a)
+#     print(10 / 0)
+#     print("No error")
+# except (NameError, ZeroDivisionError) as error:
+#     print(f"Error: {error}")
 #
-# s1 = Student()
-# w1 = Worker()
-# print(s1.height)
-# print(w1.height)
+# print("code after capsule")
 
-# class Human:
-#     def __init__(self):
-#         print("hello, мир, ")
+
+
+# def checker(text):
+#     if type(text) != str:
+#         raise TypeError(f"Sorry, we can`t work with {type(text)}, we need calss STR!")
 #
+# try:
+#     checker(5)
+# except TypeError as error:
+#     print(error)
+
+# def calculator():
+#     print("Вітаю в калькуляторі!")
+#     while True:
+#         operation = input("Виберіть дію(+,-,*,/): ")
+#         if operation not in ['+', '-', '*', '/']:
+#             print("не правильна дія.Попробуйте ще раз.")
+#             continue
+#         n1 = float(input("Перше число: "))
+#         n2 = float(input("Друге числоу: "))
+#         try:
+#             if operation == '+':
+#                 result = n1 + n2
+#             elif operation == '-':
+#                 result = n1 - n2
+#             elif operation == '*':
+#                 result = n1 * n2
+#             elif operation == '/':
+#                 result = n1 / n2
+#             print("Result: ", result)
+#         except ZeroDivisionError:
+#             print("Error: division by zero")
+#             continue
+#             print(f"{n1} {operation} {n2} = {result}")
 #
-# class Student(Human):
-#     def __init__(self):
-#         super().__init__()
-#         print("Манера крутит мир")
+# calculator()
 #
-# a = Student()
-
-# class Class1:
-#     var = 20
-#     def __init__(self):
-#         self.var = 10
+# 2 пара
 #
-# class Class2(Class1):
-#     def __init__(self):
-#         print(self.var)
-#         super().__init__()
-#         print(self.var)
-#         print(super().var)
+# import logging
 #
-# a = Class2()
-
-
-
-# class Animal:
-#     def __init__(self, name, age , species):
-#         self.name = name
-#         self.age = age
-#         self.species = species
-#
-#         def make_sound(self):
-#             print('The animal makes a sound!')
-#
-# class Dog(Animal):
-#     def __init__(self, name , age):
-#         super().__init__(name, age, species="dog")
-#
-#     def make_sound(self):
-#         print("Woof")
-#
-# class Cat(Animal):
-#     def __init__(self, name , age):
-#         super().__init__(name, age, species="cat")
-#
-#     def make_sound(self):
-#         print("Meow")
-#
-# cat = Cat("Fluffy", 2)
-# dog = Dog("Buddy", 4)
-#
-# print(cat.name)
-# print(cat.species)
-#
-# print(dog.name)
-# print(dog.species)
-#
-# cat.make_sound()
-# dog.make_sound()
-
-
-
-
-
-
-class Person:
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
-
-    def say_hello(self):
-        print(f"Hello, my name is {self.name} and I am {self.age}")
-class Student(Person):
-        def __init__(self, name, age, gender, student_id):
-            super().__init__(name, age, gender)
-            self.student_id = student_id
-
-            def say_hello(self):
-                print(f"Hello, my name is {name} and my student ID is {student_id}")
-
-class Teacher(Person):
-    def __init__(self, name, age, gender,subject):
-        super().__init__(name, age, gender)
-        self.subject = subject
-
-        def say_hello(self):
-            print(f"Hello, my name is {name} and my student ID is {self.subject}")
-
-
-
-class Engineer(Person):
-    def __init__(self, name, age, gender , company):
-        super().__init__(name, age, gender)
-        self.company = company
-
-        def say_hello(self):
-            print(f"Hello, my name is {name} and my student ID is {self.company}")
-
-s1 = Student("Dino", 22 , "Male", "1808")
-t1 = Teacher("Ann", 32 , "Female", "IT")
-e1 = Engineer("Misha", 43 , "Male", "It Step Compani")
-
-s1.say_hello()
-t1.say_hello()
-e1.say_hello()
-
+# logging.basicConfig(level=logging.DEBUG,
+# filename="logs.log", filemode="w",
+# format="[%(asctime)s] %(levelname)s %(message)s")
+# logging.debug("text")
+# logging.info("User123 succesfull logget to account 'login123'")
+# logging.info("text")
+# logging.warning("text")
+# logging.error("text")
+# logging.critical("text")
