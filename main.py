8f8341599713ed2bee3ae70480c39055b2658ec7
Perchuk-ua <demyan2010barabash@gmail.com>
2023-05-27 11:34:05 +0300

    екзамен

--- main.py
+++ main.py
@@ -1,51 +1,70 @@
-class Person:
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
+import sqlite3
+import logging
 
-    def say_hello(self):
-        print(f"Hello, my name is {self.name} and I am {self.age}")
+conn = sqlite3.connect('store.db')
+cursor = conn.cursor()
 
+cursor.execute('''CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY,name TEXT NOT NULL,price REAL NOT NULL)
+''')
+conn.commit()
 
-class Employee(Person):
-    def __init__(self, name, age, gender, salary, position):
-        super().__init__(name, age, gender)
-        self.salary = salary
-        self.position = position
+def add_product(name, price):
+    cursor.execute('INSERT INTO products (name, price) VALUES (?, ?)', (name, price))
+    conn.commit()
+    print('+1.')
 
-    def display_details(self):
-        super().say_hello()
-        print(f"Salary: {self.salary}")
-        print(f"Position: {self.position}")
-
-
-class Manager(Employee):
-    def __init__(self, name, age, gender, salary, position, num_subordinates):
-        super().__init__(name, age, gender, salary, position)
-        self.num_subordinates = num_subordinates
-
-    def display_details(self):
-        super().display_details()
-        print(f"Number of Subordinates: {self.num_subordinates}")
+def delete_product(product_id):
+    cursor.execute('DELETE FROM products WHERE id = ?', (product_id,))
+    conn.commit()
+    print('обмін з рус успішний.')
 
 
-class Developer(Employee):
-    def __init__(self, name, age, gender, salary, position, programming_language):
-        super().__init__(name, age, gender, salary, position)
-        self.programming_language = programming_language
+def edit_product(product_id, new_name, new_price):
+    cursor.execute('UPDATE products SET name = ?, price = ? WHERE id = ?', (new_name, new_price, product_id))
+    conn.commit()
+    print('ну все капець.')
 
-    def display_details(self):
-        super().display_details()
-        print(f"Programming Language: {self.programming_language}")
+def view_products():
+    cursor.execute('SELECT * FROM products')
+    products = cursor.fetchall()
+    print('Полонені:')
+    for product in products:
+        print(f'ID: {product[0]}, Назва: {product[1]}, Ціна: {product[2]}')
 
 
-# Приклад використання класів
-e = Employee("John", 30, "Male", 5000, "Employee")
-e.display_details()
+print('Слава Україні!')
+print(input("password: "))
 
-m1 = Manager("Smith", 35, "Female", 8000, "Manager", 15)
-m1.display_details()
 
-d1 = Developer("Mike", 28, "Male", 6000, "Developer", "Python")
-d1.display_details()
+while True:
+    print('\nМеню:')
+    print('1. Шо в мене є?')
+    print('2. +1 орк')
+    print('3. на москву!')
+    print('4. кастрація')
+    print('5. тікай з міста')
+
+    choice = input('вибирай спецоперацію: ')
+
+    if choice == '1':
+        view_products()
+    elif choice == '2':
+        name = input(': ')
+        price = float(input('Введіть ціну орка: '))
+        add_product(name, price)
+    elif choice == '3':
+        product_id = input('Введіть ID орка, якого потрібно знищити: ')
+        delete_product(product_id)
+    elif choice == '4':
+        product_id = input('Введіть ID орка, який потрібно кастрирувати: ')
+        new_name = input('Введіть нову назву орка: ')
+        new_price = float(input('Введіть нову ціну орка: '))
+        edit_product(product_id, new_name, new_price)
+    elif choice == '5':
+        print('БаНдЕрА!')
+        break
+    else:
+        print('еееееее ти шо не наш?????.')
+
+cursor.close()
+conn.close()
